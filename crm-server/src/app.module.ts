b991fdc7ec7bb4201e7dff516faa08cb2135ce73
MartinBrown <griver679@gmail.com>
2022-05-10 10:25:13 +0300

    feat: move modules to dir modules

--- crm-server/src/app.module.ts
+++ crm-server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { UserController } from './user/user.controller';
+import { AuthController } from './modules/auth/auth.controller';
+import { UserController } from './modules/user/user.controller';
 
 @Module({
   controllers: [AuthController, UserController],
