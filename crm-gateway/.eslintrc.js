7a3c40a15da0f45d6a4525ece94a5d5e45dbdc69
Andrew Chemes <andrey.chemes@gmail.com>
2022-06-10 17:08:28 +0300

    some fix eslint and prettier

--- crm-gateway/.eslintrc.js
+++ crm-gateway/.eslintrc.js
@@ -2,18 +2,31 @@ module.exports = {
   'env': {
     'browser': true,
     'es2021': true,
+    'node': true,
+    'jest': true,
   },
+  'ignorePatterns': ['.eslintrc.js'],
   'extends': [
     'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 'latest',
     'sourceType': 'module',
+    'project': 'tsconfig.json',
+    'sourceType': 'module',
   },
+  'root': 'true',
   'plugins': [
     '@typescript-eslint',
   ],
   'rules': {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
