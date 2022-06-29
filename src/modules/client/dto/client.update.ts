0d52fbea3aa9aaf466c511c11943305e423e4e40
BogdanRadetskiy <bogdan.Radetskyi@fivesysdev.com>
2022-06-29 13:53:29 +0300

    fix: code

--- src/modules/client/dto/client.update.ts
+++ src/modules/client/dto/client.update.ts
@@ -6,17 +6,4 @@ export class ClientUpdateDto {
   @IsString()
   @IsNotEmpty()
   name: string;
-
-  @ApiProperty({ description: 'Client ID', example: '9878f24c-6b96-43cd-a1c4-69c66c875827' })
-  @IsUUID(4)
-  clientId: string;
-
-  @ApiProperty({
-    type: String,
-    isArray: true,
-    example: ['9878f24c-6b96-43cd-a1c4-69c66c875827'],
-    description: 'Array of performer ids',
-  })
-  @IsUUID(4, { each: true })
-  performerIds: String[];
 }
